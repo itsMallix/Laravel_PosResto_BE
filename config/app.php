7210f8d5807743adb6a258d49f410daa2dfc6a20
itsMallix <amandamaulana00@gmail.com>
2024-02-09 22:22:06 +0700

    docs : add login

--- config/app.php
+++ config/app.php
@@ -160,14 +160,14 @@ return [
          * Package Service Providers...
          */
         // Fortivy
-        App\Providers\FortifyServiceProvider::class,
 
         /*
-         * Application Service Providers...
-         */
+        * Application Service Providers...
+        */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.dashboard');
+    return view('pages.auth.login');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function(){
+        return view('pages.dashboard');
+    })-> name('home');
+});
 
